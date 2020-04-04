03b5d64081681e8560d96d8d62c02b9af84bd757
Isac Sund <isac.sund@outlook.com>
2020-04-04 14:29:52 +0300

    Use window.location instead of hardcoded urls

--- frontend/src/Constants.js
+++ frontend/src/Constants.js
@@ -1,9 +1,9 @@
 const prod = {
-  WS_URL: "ws://example.com"
+  WS_URL: `ws://${window.location.host}`,
 };
 
 const dev = {
-  WS_URL: "ws://localhost:" + (process.env.SERVER_PORT || 8080)
+  WS_URL: `ws://${window.location.hostname}:${process.env.SERVER_PORT || 8080}`,
 };
 
 export const config = process.env.NODE_ENV === "development" ? dev : prod;
