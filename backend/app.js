5d412036d490d320b267c915735c761d9a2c01fc
Johan <johanjern95@gmail.com>
2020-04-05 11:50:12 +0300

    use strict, db funcs to separate file

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,15 @@
+"use strict";
+
 const WebSocket = require("ws");
 const express = require("express");
 const http = require("http");
+
+const {
+  addDrawEvent,
+  getAllDrawEvents,
+  addWriteEvent,
+  getLatestWriteEvents
+} = require("./db");
 
 const app = express();
 app.use("/", express.static("../frontend/build/"));
@@ -9,97 +18,7 @@ app.use("*", express.static("../frontend/build/"));
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
-const sqlite3 = require("sqlite3").verbose();
-
-const db = new sqlite3.Database("./app.db", (err) => {
-  if (err) {
-    console.error(err.message);
-    process.exit();
-  }
-  console.log("Connected to the SQlite database.");
-});
-
-db.serialize(() => {
-  db.run(
-    "create table if not exists eventTypes(" +
-      "typeId integer primary key," +
-      "type text unique not null" +
-      ");",
-    (err) => {
-      if (err) console.error(err);
-    }
-  );
-  db.run(
-    "insert or ignore into eventTypes(typeId, type) values (0, 'DRAW');",
-    (err) => {
-      if (err) console.error(err);
-    }
-  );
-  db.run(
-    "insert or ignore into eventTypes(typeId, type) values (1, 'WRITE');",
-    (err) => {
-      if (err) console.error(err);
-    }
-  );
-
-  db.run(
-    "create table if not exists events (" +
-      "id integer primary key," +
-      "url text," +
-      "eventType int," +
-      "event text," +
-      "foreign key(eventType) references eventTypes(typeId)" +
-      ");",
-    (err) => {
-      if (err) console.error(err);
-    }
-  );
-});
-
-const addDrawEvent = (url, event) => {
-  db.run(
-    "insert into events(url, event, eventType) values(?, ?, 0);",
-    [url, event],
-    (err) => {
-      if (err) console.log(err);
-    }
-  );
-};
-
-const getAllDrawEvents = (url, callback) => {
-  db.all(
-    "select event from events where url = ? and eventType = 0 order by id asc;",
-    url,
-    (err, rows) => {
-      if (err) console.log(err);
-      const rowsString = "[" + rows.map((r) => r.event).join(",") + "]";
-      callback(rowsString);
-    }
-  );
-};
-
-const addWriteEvent = (url, event) => {
-  db.run(
-    "insert into events(url, event, eventType) values(?, ?, 1);",
-    [url, event],
-    (err) => {
-      if (err) console.log(err);
-    }
-  );
-};
-
-const getLatestWriteEvents = (url, writeIdx, callback) => {
-  db.all(
-    "select id, event from events where url = ? and id > ? and eventType = 1 order by id asc;",
-    [url, writeIdx],
-    (err, rows) => {
-      if (err) console.log(err);
-      callback(rows);
-    }
-  );
-};
-
-clients = {};
+const clients = {};
 
 wss.on("connection", (ws) => {
   ws.on("message", (msgString) => {
@@ -156,7 +75,7 @@ wss.on("connection", (ws) => {
 
 const intervalTimeout = setInterval(() => {
   Object.keys(clients).forEach((url) => {
-    urlClients = clients[url];
+    const urlClients = clients[url];
     urlClients.forEach(({ ws, writeIdx }, i) => {
       getLatestWriteEvents(url, writeIdx, (rows) => {
         if (rows.length > 0) {
---
+++ backend/db.js
@@ -0,0 +1,90 @@
+"use strict";
+const sqlite3 = require("sqlite3").verbose();
+
+const db = new sqlite3.Database("./app.db", (err) => {
+  if (err) {
+    console.error(err.message);
+    process.exit();
+  }
+  console.log("Connected to the SQlite database.");
+});
+
+db.serialize(() => {
+  db.run(
+    "create table if not exists eventTypes(" +
+      "typeId integer primary key," +
+      "type text unique not null" +
+      ");",
+    (err) => {
+      if (err) console.error(err);
+    }
+  );
+  db.run(
+    "insert or ignore into eventTypes(typeId, type) values (0, 'DRAW');",
+    (err) => {
+      if (err) console.error(err);
+    }
+  );
+  db.run(
+    "insert or ignore into eventTypes(typeId, type) values (1, 'WRITE');",
+    (err) => {
+      if (err) console.error(err);
+    }
+  );
+
+  db.run(
+    "create table if not exists events (" +
+      "id integer primary key," +
+      "url text," +
+      "eventType int," +
+      "event text," +
+      "foreign key(eventType) references eventTypes(typeId)" +
+      ");",
+    (err) => {
+      if (err) console.error(err);
+    }
+  );
+});
+
+exports.addDrawEvent = (url, event) => {
+  db.run(
+    "insert into events(url, event, eventType) values(?, ?, 0);",
+    [url, event],
+    (err) => {
+      if (err) console.log(err);
+    }
+  );
+};
+
+exports.getAllDrawEvents = (url, callback) => {
+  db.all(
+    "select event from events where url = ? and eventType = 0 order by id asc;",
+    url,
+    (err, rows) => {
+      if (err) console.log(err);
+      const rowsString = "[" + rows.map((r) => r.event).join(",") + "]";
+      callback(rowsString);
+    }
+  );
+};
+
+exports.addWriteEvent = (url, event) => {
+  db.run(
+    "insert into events(url, event, eventType) values(?, ?, 1);",
+    [url, event],
+    (err) => {
+      if (err) console.log(err);
+    }
+  );
+};
+
+exports.getLatestWriteEvents = (url, writeIdx, callback) => {
+  db.all(
+    "select id, event from events where url = ? and id > ? and eventType = 1 order by id asc;",
+    [url, writeIdx],
+    (err, rows) => {
+      if (err) console.log(err);
+      callback(rows);
+    }
+  );
+};
