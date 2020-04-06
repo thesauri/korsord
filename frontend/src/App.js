4e28cd6722a991ad056270b9751cf1735e14969f
Walter Berggren <walter.berggren@gmail.com>
2020-04-06 18:48:52 +0300

    Redirect to the home page on non-existing games

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,25 @@
 import React, { useState, useEffect } from "react";
 import Crossword from "./Crossword";
 import "./App.css";
-import { useParams } from "react-router-dom";
+import { useParams, useHistory } from "react-router-dom";
 import { config } from "./Constants";
 import { getCrosswordForGame } from "./restApi";
 
 function App() {
   const [crosswordImage, setCrosswordImage] = useState(null);
   const { url } = useParams();
+  const history = useHistory();
 
   useEffect(() => {
     const getGameAndImage = async () => {
-      const crossword = await getCrosswordForGame(url);
+      let crossword;
+      try {
+        crossword = await getCrosswordForGame(url);
+      } catch (error) {
+        console.error("Game not found, redirecting to home page");
+        history.push("/");
+        return;
+      }
       const crosswordImage = new Image();
       crosswordImage.src = `${config.BACKEND_URL}/${crossword.imageUrl}`;
       crosswordImage.onload = () => {
