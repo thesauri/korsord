53d95abff7a7d7117fb534662d8a1f9edcd14c44
Walter Berggren <walter.berggren@gmail.com>
2020-04-06 15:12:27 +0300

    POST to start games

--- backend/api.js
+++ backend/api.js
@@ -1,5 +1,6 @@
 "use strict";
 const router = require("express").Router();
+const jsonParser = require("express").json();
 
 const enableCORS = (res) => {
   // TODO: Update the origin list once we use the API for something important
@@ -15,21 +16,35 @@ router.use((req, res, next) => {
   next();
 });
 
+router.use(jsonParser);
+
 router.get("/crosswords", (req, res) => {
   const response = [
     {
+      crosswordId: 2,
       newspaper: "HBL",
       date: "2020-04-03",
       image_url: "uploads/2020-04-03/crossword.jpg",
       metadata_url: "uploads/2020-04-04/squares.json"
     },
     {
+      crosswordId: 1,
       newspaper: "HBL",
       date: "2020-03-27",
       image_url: "uploads/2020-03-27/crossword.jpg",
       metadata_url: "uploads/2020-03-27/squares.json"
     }
   ];
+  res.contentType = "application/json";
+  res.send(JSON.stringify(response));
+});
+
+router.post("/game", (req, res) => {
+  const { crosswordId } = req.body;
+  const response = {
+    gameId: "test-fox-blue",
+    crosswordId: crosswordId
+  };
   res.contentType = "application/json";
   res.send(JSON.stringify(response));
 });
--- frontend/src/CrosswordSelector.jsx
+++ frontend/src/CrosswordSelector.jsx
@@ -2,22 +2,43 @@ import React, { useState, useEffect } from "react";
 import moment from "moment";
 import { config } from "./Constants";
 import "./CrosswordSelector.css";
+import { useHistory } from "react-router-dom";
 
 const CrosswordSelector = () => {
   const crosswords = useCrosswords();
+  const [selectedCrossword, setSelectedCrossword] = useState(null);
+  const history = useHistory();
+
+  const onNewGame = (crosswordId) => {
+    if (selectedCrossword !== null) {
+      return;
+    }
+    const createAndStartNewGame = async () => {
+      const { gameId } = await createNewGame(crosswordId);
+      history.push(`/game/${gameId}`);
+    };
+    setSelectedCrossword(crosswordId);
+    createAndStartNewGame();
+  };
+
   return (
     <div className="crosswordselector">
       {crosswords.map((crossword) => (
         <a
           className="crosswordselector-card"
-          key={`${crossword.newspaper} ${crossword.date}`}
+          key={crossword.crosswordId}
+          onClick={() => onNewGame(crossword.crosswordId)}
         >
           <img
             className="crosswordselector-card-image"
             src={`${config.BACKEND_URL}/${crossword.image_url}`}
           />
           <p className="crosswordselector-card-link" href="#">
-            {crossword.newspaper} {moment(crossword.date).format("DD.MM.YYYY")}
+            {selectedCrossword === crossword.crosswordId
+              ? "Öppnar..."
+              : `${crossword.newspaper} ${moment(crossword.date).format(
+                  "DD.MM.YYYY"
+                )}`}
           </p>
         </a>
       ))}
@@ -40,6 +61,21 @@ const useCrosswords = () => {
   }, []);
 
   return crosswords;
+};
+
+const createNewGame = async (crosswordId) => {
+  const requestBody = JSON.stringify({
+    crosswordId
+  });
+  const response = await fetch(`${config.BACKEND_URL}/api/game`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: requestBody
+  });
+  const responseData = await response.json();
+  return responseData;
 };
 
 export default CrosswordSelector;
