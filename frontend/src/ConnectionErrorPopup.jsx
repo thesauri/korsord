089737f916628fa455b5b55dde736e5bbdedaabf
Anton Eriksson <eriksson.e.anton@gmail.com>
2020-04-13 17:27:43 +0300

    Show error popup if socket connection is closed

---
+++ frontend/src/ConnectionErrorPopup.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./ConnectionErrorPopup.css";
+
+const ErrorMessagePopup = (props) => (
+  <div className="dim-background">
+    <div className="error-popup">
+      <i class="fas fa-exclamation-triangle fa-3x"></i>
+      <p className="error-header">{props.title}</p>
+      <p className="error-message">{props.message}</p>
+    </div>
+  </div>
+);
+
+export const ConnectionErrorPopup = () => (
+  <ErrorMessagePopup
+    title="Anslutningen avbröts"
+    message="Prova att ladda om sidan pånytt."
+  />
+);
